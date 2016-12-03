b485150b5b0f640f981c197938ce828471db329e
Tyjameslundgames <tyjameslund@gmail.com>
2016-12-02 18:30:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
+
+// The key press function looks for a keypress on the html element (while page loaded)
 $("html").keypress(function() {
   console.log("Handler for .keypress() called.");
 });
+
+
+
