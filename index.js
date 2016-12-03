ac797631d43ef5d374320c0c89fd529d5fb36331
Tyjameslundgames <tyjameslund@gmail.com>
2016-12-02 18:40:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
+// Grab some Linux C files from the Linux repository 
 
 // The key press function looks for a keypress on the html element (while page loaded)
 $("html").keypress(function() {
+  // use a loop to add on to the $('.output') element a word of the C files, use the split(" ") function to split the string into words
   console.log("Handler for .keypress() called.");
 });
 
